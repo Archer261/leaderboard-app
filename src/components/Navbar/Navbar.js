fd25b9d81b87788fddc8e3c94a2abea881fe2d87
Joseph Archer <viewtiful.kja@gmail.com>
2023-01-30 09:52:38 -0500

    update a value

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { client } from '../../client'
 
 import './Navbar.css'
 import PotCount from '../PotCount/PotCount'
 import images from '../../assets/images'
 
 const Navbar = () => {
+
+    const [potCountNumber, setPotCountNumber] = useState([]);
+
+    useEffect(() => {
+        const potCountNumberQuery = '*[_type == "tracking"]';
+
+        client.fetch(potCountNumberQuery)
+            .then((data) => {
+                setPotCountNumber(data);
+            })
+    }, [])
+
+
     return (
         <div className='nav-grid'>
             <img src={images.logo} className='header-wrapper-img' />
             <img src={images.Leaderboard} className='leaderboard-img' />
-            <PotCount />
+            <PotCount potNumber={potCountNumber.potCount} />
         </div>
     )
 }
--- src/components/PotCount/PotCount.js
+++ src/components/PotCount/PotCount.js
@@ -3,11 +3,12 @@ import CountUp from 'react-countup';
 import images from '../../assets/images';
 import './PotCount.css'
 
-const PotCount = () => {
+const PotCount = (potNumber) => {
+    console.log(potNumber)
     return (
         <div className='potCount-container'>
             <img src={images.potImg} alt="prize-pot" />
-            <h1>$<CountUp end={210} /></h1>
+            <h1>$<CountUp end={270} /></h1>
         </div>
     )
 }
